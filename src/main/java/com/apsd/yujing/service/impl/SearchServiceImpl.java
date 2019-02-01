925e75bb31555553bad4d5944b8efdc07dd12e47
daji <www.137000620@qq.com>
2019-02-01 13:56:49 +0800

    search add typeName

--- src/main/java/com/apsd/yujing/service/impl/SearchServiceImpl.java
+++ src/main/java/com/apsd/yujing/service/impl/SearchServiceImpl.java
@@ -1,11 +1,7 @@
 package com.apsd.yujing.service.impl;
 
-import com.apsd.yujing.entiy.CaseKind;
-import com.apsd.yujing.entiy.News;
-import com.apsd.yujing.entiy.Product;
-import com.apsd.yujing.repository.CaseKindRepository;
-import com.apsd.yujing.repository.NewsRepository;
-import com.apsd.yujing.repository.ProductRepository;
+import com.apsd.yujing.entiy.*;
+import com.apsd.yujing.repository.*;
 import com.apsd.yujing.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,8 +18,17 @@ public class SearchServiceImpl implements SearchService {
     private CaseKindRepository caseKindRepository;
     @Autowired
     private NewsRepository newsRepository;
+    @Autowired
+    private ProductTypeRepository productTypeRepository;
+    @Autowired
+    private CaseTypeRepository caseTypeRepository;
+
     @Override
     public Map getSearch(String key,int flag) {
+        boolean _flag=false;
+        if(flag==1){
+            _flag=true;
+        }
         key="%"+key+"%";
         Map map =new HashMap();
         List<Product> productList = productRepository.getProductsByFlagAndNameContainingOrTextContaining(flag,key, key);
@@ -33,9 +38,13 @@ public class SearchServiceImpl implements SearchService {
         List<News> newsList = newsRepository.getNewsByFlagAndStageContainingOrTitleContaining(flag,key, key);
         System.out.println(newsList);
         if(productList.size()!=0){
+            List<ProductType> productTypeList = productTypeRepository.findAllByFlag(_flag);
+            this.setTypeNameToEachProduct(productList,productTypeList);
             map.put("product",productList);
         }
         if(casekindList.size()!=0){
+            List<CaseType> caseTypeList = caseTypeRepository.findAllByFlag(_flag);
+            this.setTypeNameToEachCaseKind(casekindList,caseTypeList);
             map.put("caseKind",casekindList);
         }
         if(newsList.size()!=0){
@@ -43,5 +52,25 @@ public class SearchServiceImpl implements SearchService {
         }
         System.out.println(map);
         return map;
+    }
+    private List<Product> setTypeNameToEachProduct(List<Product> productList,List<ProductType> productTypeList){
+        for(Product product:productList){
+            for(ProductType productType:productTypeList){
+                if(product.getType()==productType.getId()){
+                    product.setTypeName(productType.getTypeName());
+                }
+            }
+        }
+        return productList;
+    }
+    private List<CaseKind> setTypeNameToEachCaseKind(List<CaseKind> caseKindList,List<CaseType> caseTypeList){
+        for(CaseKind caseKind:caseKindList){
+            for(CaseType caseType:caseTypeList){
+                if(caseKind.getType()==caseType.getId()){
+                    caseKind.setTypeName(caseType.getType_name());
+                }
+            }
+        }
+        return caseKindList;
     }
 }
